6f8bd605a14706031ae8a11b195d92d3db25f355
joeyholzman <91689192+joeyholzman@users.noreply.github.com>
2022-11-28 15:25:11 -0500

    Update Calendar.php

--- Calendar.php
+++ Calendar.php
@@ -1,135 +1,148 @@
-<?php
-date_default_timezone_set('America/New_York');
-
-// Get prev & next month
-if (isset($_GET['ym'])) {
-    $ym = $_GET['ym'];
-} else {
-    // This month
-    $ym = date('Y-m');
-}
-
-// Check format
-$timestamp = strtotime($ym . '-01');  // the first day of the month
-if ($timestamp === false) {
-    $ym = date('Y-m');
-    $timestamp = strtotime($ym . '-01');
-}
-
-// Today (Format:2018-08-8)
-$today = date('Y-m-j');
-
-// Title (Format:August, 2018)
-$title = date('F, Y', $timestamp);
-
-// Create prev & next month link
-$prev = date('Y-m', strtotime('-1 month', $timestamp));
-$next = date('Y-m', strtotime('+1 month', $timestamp));
-
-// Number of days in the month
-$day_count = date('t', $timestamp);
-
-// 1:Mon 2:Tue 3: Wed ... 7:Sun
-$str = date('N', $timestamp);
-
-// Array for calendar
-$weeks = [];
-$week = '';
-
-// Add empty cell(s)
-$week .= str_repeat('<td></td>', $str - 1);
-
-for ($day = 1; $day <= $day_count; $day++, $str++) {
-
-    $date = $ym . '-' . $day;
-
-    if ($today == $date) {
-        $week .= '<td class="today">';
-    } else {
-        $week .= '<td>';
-    }
-    $week .= $day . '</td>';
-
-    // Sunday OR last day of the month
-    if ($str % 7 == 0 || $day == $day_count) {
-
-        // last day of the month
-        if ($day == $day_count && $str % 7 != 0) {
-            // Add empty cell(s)
-            $week .= str_repeat('<td></td>', 7 - $str % 7);
-        }
-
-        $weeks[] = '<tr>' . $week . '</tr>';
-
-        $week = '';
-    }
-}
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="utf-8">
-    <title>PHP Calendar</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <style>
-        .container {
-            font-family: 'Montserrat', sans-serif;
-            margin: 60px auto;
-        }
-        .list-inline {
-            text-align: center;
-            margin-bottom: 30px;
-        }
-        .title {
-            font-weight: bold;
-            font-size: 26px;
-        }
-        th {
-            text-align: center;
-        }
-        td {
-            height: 100px;
-        }
-        th:nth-of-type(6), td:nth-of-type(6) {
-            color: blue;
-        }
-        th:nth-of-type(7), td:nth-of-type(7) {
-            color: red;
-        }
-        .today {
-            background-color: ghostwhite;
-        }
-    </style>
+<style>
+* {box-sizing: border-box;}
+ul {list-style-type: none;}
+body {font-family: Verdana, sans-serif;}
+
+.month {
+  padding: 70px 25px;
+  width: 100%;
+  background: #1abc9c;
+  text-align: center;
+}
+
+.month ul {
+  margin: 0;
+  padding: 0;
+}
+
+.month ul li {
+  color: white;
+  font-size: 20px;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+}
+
+.month .prev {
+  float: left;
+  padding-top: 10px;
+}
+
+.month .next {
+  float: right;
+  padding-top: 10px;
+}
+
+.weekdays {
+  margin: 0;
+  padding: 10px 0;
+  background-color: #ddd;
+}
+
+.weekdays li {
+  display: inline-block;
+  width: 13.6%;
+  color: #666;
+  text-align: center;
+}
+
+.days {
+  padding: 10px 0;
+  background: #eee;
+  margin: 0;
+}
+
+.days li {
+  list-style-type: none;
+  display: inline-block;
+  width: 13.6%;
+  text-align: center;
+  margin-bottom: 5px;
+  font-size:12px;
+  color: #777;
+}
+
+.days li .active {
+  padding: 5px;
+  background: #1abc9c;
+  color: white !important
+}
+
+/* Add media queries for smaller screens */
+@media screen and (max-width:720px) {
+  .weekdays li, .days li {width: 13.1%;}
+}
+
+@media screen and (max-width: 420px) {
+  .weekdays li, .days li {width: 12.5%;}
+  .days li .active {padding: 2px;}
+}
+
+@media screen and (max-width: 290px) {
+  .weekdays li, .days li {width: 12.2%;}
+}
+</style>
 </head>
 <body>
-    <div class="container">
-        <ul class="list-inline">
-            <li class="list-inline-item"><a href="?ym=<?= $prev; ?>" class="btn btn-link">&lt; prev</a></li>
-            <li class="list-inline-item"><span class="title"><?= $title; ?></span></li>
-            <li class="list-inline-item"><a href="?ym=<?= $next; ?>" class="btn btn-link">next &gt;</a></li>
-        </ul>
-        <p class="text-right"><a href="calendar.php">Today</a></p>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>M</th>
-                    <th>T</th>
-                    <th>W</th>
-                    <th>T</th>
-                    <th>F</th>
-                    <th>S</th>
-                    <th>S</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    foreach ($weeks as $week) {
-                        echo $week;
-                    }
-                ?>
-            </tbody>
-        </table>
-    </div>
+
+<h1>Welcome, </h1>
+
+<div class="month">
+  <ul>
+    <li class="prev">&#10094;</li>
+    <li class="next">&#10095;</li>
+    <li>
+      August<br>
+      <span style="font-size:18px">2021</span>
+    </li>
+  </ul>
+</div>
+
+<ul class="weekdays">
+  <li>Mo</li>
+  <li>Tu</li>
+  <li>We</li>
+  <li>Th</li>
+  <li>Fr</li>
+  <li>Sa</li>
+  <li>Su</li>
+</ul>
+
+<ul class="days">
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+  <li>4</li>
+  <li>5</li>
+  <li>6</li>
+  <li>7</li>
+  <li>8</li>
+  <li>9</li>
+  <li><span class="active">10</span></li>
+  <li>11</li>
+  <li>12</li>
+  <li>13</li>
+  <li>14</li>
+  <li>15</li>
+  <li>16</li>
+  <li>17</li>
+  <li>18</li>
+  <li>19</li>
+  <li>20</li>
+  <li>21</li>
+  <li>22</li>
+  <li>23</li>
+  <li>24</li>
+  <li>25</li>
+  <li>26</li>
+  <li>27</li>
+  <li>28</li>
+  <li>29</li>
+  <li>30</li>
+  <li>31</li>
+</ul>
+
 </body>
 </html>
