e7f8578b3385a956275507303a352b2536ce724b
joeyholzman <91689192+joeyholzman@users.noreply.github.com>
2022-11-30 15:14:49 -0500

    Update dayview.php

--- dayview.php
+++ dayview.php
@@ -1,36 +1,44 @@
-<!-- Task page where users can enter new items on agenda -->
-<!DOCTYPE html>
 <html>
-<head>
-<title>ToDo List</title>
-</head>
-<h1> To-Do List </h1>
 <body>
-<div class="list">
-<ul class="items">
-	<li>
-<span class="item">250 Homework</span>
-	</li>
 
-	<li>
-<span class="item">Study for finals</span>
-	</li>
+  <form action="dayview.php" method="post">
+  <input type = "submit" name = "back" value = "Back to Calendar"></button>
+  </form>
 
-	<li>
-<span class="item">Laundry</span>
-	</li>
+<?php
 
-	<li>
-<span class="item">Exercise</span>
-	</li>
+require_once('config.php');
 
-</ul>
-<form action = "Tasks.php" method="POST">
-	<input type= "text" class="input" placeholder="add item" required>
-	<input type= "submit" class= "submit" name = "add" value = "add"/>
-</form>
-</div>
+if(isset($_POST["submit"])){
+  if(isset($_POST["day"])){
+    $day = intval($_POST["day"]);
+
+    echo "<h1>November ".$day.", 2022</h1>";
+
+    $user = mysqli_real_escape_string($conn,$_POST['user']);
+
+    $query = "SELECT title, descrip FROM $user WHERE day = $day";
+
+    $result = mysqli_query($conn, $query);
+
+    echo "<table>";
+    while($row = mysqli_fetch_array($result)){
+      echo "hi";
+      echo($row['title']);
+      //echo "<tr><td>".htmlspecialchars($row['title'])."</td><td>".htmlspecialchars($row['descrip'])."</td></tr>";
+      }
+    echo "</table>";
+    }
+    else{
+      echo "Please select a day!";
+    }
+  }
+
+if(isset($_POST["back"])){
+  header("Location: http://localhost:8888/Calendar.php");
+}
+
+?>
+
 </body>
-
-</head>
 </html>
